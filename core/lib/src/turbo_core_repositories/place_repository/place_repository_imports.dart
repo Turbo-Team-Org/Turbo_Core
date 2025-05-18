1183c329fe8e2b4ac38c4423066ef8cc2b9df348
DavidIsuCorp <david.zequeira@isucorp.ca>
2025-05-17 21:42:36 -0400

    adding imports that were not included

--- core/lib/src/turbo_core_repositories/place_repository/place_repository_imports.dart
+++ core/lib/src/turbo_core_repositories/place_repository/place_repository_imports.dart
@@ -1,3 +1,6 @@
+export 'package:core/src/turbo_core_repositories/place_repository/models/feed/feed.dart';
+export 'package:core/src/turbo_core_repositories/place_repository/models/offer/offer.dart';
 export 'package:core/src/turbo_core_repositories/place_repository/models/place/place.dart';
+export 'package:core/src/turbo_core_repositories/place_repository/models/schedule/schedule.dart';
 export 'package:core/src/turbo_core_repositories/place_repository/place_repository.dart';
 export 'package:core/src/turbo_core_repositories/place_repository/service/place_service.dart';
